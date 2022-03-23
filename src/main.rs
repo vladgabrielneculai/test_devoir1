39ad232196b830dcdda933420473656db87abc35
Vlad Neculai <vlad.neculai@stud.fils.upb.ro>
2022-03-23 10:44:49 +0200

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-use std::env::{args, self};
+use std::{env::{args, self}, fs};
 
 fn pwd(){
     let dir = env::current_dir().unwrap();
-    println!("{:?}",dir);
+    println!("{}",dir.display());
 }
 
-fn echo(){
-    println!("{}", env::args().skip(1).collect()::Vec<_>>().join(" "));
+// fn echo(){
+//     let optiune:String = param[2];
+//     let parametru:String = param[3];
+        
 
+// }
+
+fn mkdir(nume:&String){
+    let r = fs::create_dir_all(nume);
+    match r {
+        Err(e) => println!("error creating {}: {}", nume, e),
+        Ok(_) => println!("created {}: OK", nume),
+    }
 }
 fn main() {
     let param: Vec<String> = args().collect();
     
     let comanda = &param[1];
-    // let nume = &param[2];
-
+    let nume = &param[2];
+  
     match comanda.as_str(){
         "pwd" => pwd(),
-        "echo" => echo(),
+        //"echo" => echo(),
+        "mkdir" => mkdir(nume),
         _=>std::process::exit(-1),
     }
 }
